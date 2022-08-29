0e4814b0366ba3866a4bb3e1c7fa2e6ed9ea60aa
usford <lukichev227@mail.ru>
2022-08-29 16:56:57 +0300

    Initial commit

---
+++ cleverenceTask2/AsyncCaller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace cleverenceTask2
+{
+    public class AsyncCaller
+    {
+        public AsyncCaller(EventHandler<CustomEventArgs> eventHandler)
+        {
+            _myEventHandler = eventHandler;
+        }
+        private EventHandler<CustomEventArgs> _myEventHandler;
+        
+        public bool Invoke(int timeout, object? sender, CustomEventArgs e)
+        {
+            var source = new CancellationTokenSource();
+            CancellationToken token = source.Token;
+
+            var stateInvoke = false;
+
+            Task taskInvoke = new Task(() =>
+            {
+                e.token = token;
+                _myEventHandler.Invoke(sender, e);
+                stateInvoke = true;
+            }, token);
+           
+            taskInvoke.Start();
+            taskInvoke.Wait(timeout);
+            source.Cancel();
+
+            return stateInvoke;
+        }
+    } 
+}
---
+++ cleverenceTask2/Program.cs
@@ -0,0 +1,27 @@
+﻿using cleverenceTask2;
+
+var h = new EventHandler<CustomEventArgs>(MyEventHandler!);
+var ac = new AsyncCaller(h);
+
+bool compeltedOK1 = ac.Invoke(1000, null, new CustomEventArgs());
+Console.WriteLine($"Не успел выполниться: {compeltedOK1}");
+
+bool compeltedOK2 = ac.Invoke(3000, null, new CustomEventArgs());
+Console.WriteLine($"Успел выполниться: {compeltedOK2}");
+
+void MyEventHandler (object sender, CustomEventArgs e)
+{
+    var token = e.token;
+
+    //Симуляция работы
+    Thread.Sleep(2000);
+
+    if (token.IsCancellationRequested) return;
+
+    Console.WriteLine("EVENT HANDLER FINISHED");
+}
+
+public class CustomEventArgs : EventArgs
+{
+    public CancellationToken token { get; set; } = default;
+}
